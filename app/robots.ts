c9eaf178b45771679351487e694f9871ad4557a9
kfkkail <kail.keusch@hey.com>
2026-01-08 16:15:06 -0500

    Remove sitemap

--- app/robots.ts
+++ app/robots.ts
@@ -6,7 +6,6 @@ export default function robots(): MetadataRoute.Robots {
       userAgent: '*',
       allow: '/',
     },
-    sitemap: `${process.env.NEXT_PUBLIC_SITE_URL || 'https://ashfordhillsgranger.org'}/sitemap.xml`,
   };
 }
 
